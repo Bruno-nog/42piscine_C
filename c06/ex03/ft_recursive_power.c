cb8ce7c57e8d8501040c36d8a263e59897022183
Bruno Nogueira De Queiroz <bqueiroz.nog@gmail.com>
2025-04-14 13:02:28 -0300

    Deleted .git from rush-01

--- c06/ex03/ft_recursive_power.c
+++ c06/ex03/ft_recursive_power.c
@@ -6,17 +6,17 @@
 /*   By: brunogue <brunogue@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/31 08:26:42 by brunogue          #+#    #+#             */
-/*   Updated: 2025/04/14 12:59:12 by brunogue         ###   ########.fr       */
+/*   Updated: 2025/04/14 13:01:58 by brunogue         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_recursive_power(int nb, int power)
+int	ft_recursive_power(int nbr, int power)
 {
 	int	power;
 
 	power = 0;
 	if (power <= 0)
 		return (0);
-	if (nb == 0 && power == 0)
+	if (nbr == 0 && power == 0)
 		return (1);
 }
